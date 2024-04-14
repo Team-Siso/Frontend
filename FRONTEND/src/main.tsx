49fa7eecb4bae4098cf5ec599691914bd5b5b362
Shin-Sujin <tnwls5621@tukorea.ac.kr>
2024-04-14 23:43:28 +0900

    fix : calendar 파일 이동

--- FRONTEND/src/main.tsx
+++ FRONTEND/src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import Sample from "./calendar.tsx";
+import Sample from "./components/calendar";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<Sample />);
