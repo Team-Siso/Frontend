52b9b5c6bf72ca783932378dcc95fc30371d3e20
Shin-Sujin <tnwls5621@tukorea.ac.kr>
2024-04-14 19:53:42 +0900

    design : '일'없애기

--- FRONTEND/src/calendar.tsx
+++ FRONTEND/src/calendar.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
-
+import moment from "moment";
 import "./calendar.css";
 
 type ValuePiece = Date | null;
@@ -10,7 +10,18 @@ type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 function Sample() {
   const [value, onChange] = useState<Value>(new Date());
+  const [nowDate, setNowDate] = useState("날짜");
+  const [isOpen, setIsOpen] = useState(false);
 
+  const handleToggleCalendar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleDateChange = (selectedDate) => {
+    onChange(selectedDate);
+    setIsOpen(false);
+    setNowDate(moment(selectedDate).format("YYYY년 MM월 DD일"));
+  };
   return (
     <div>
       <header>
@@ -18,7 +29,12 @@ function Sample() {
       </header>
       <div>
         <main>
-          <Calendar onChange={onChange} value={value} />
+          {/* <Calendar onChange={onChange} value={value} /> */}
+          <Calendar
+            onChange={handleDateChange}
+            value={value}
+            formatDay={(locale, date) => moment(date).format("DD")}
+          ></Calendar>
         </main>
       </div>
     </div>
