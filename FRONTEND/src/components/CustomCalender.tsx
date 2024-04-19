43d3b8e9a3766706a093180afb2f85f617c864c3
Shin-Sujin <tnwls5621@tukorea.ac.kr>
2024-04-19 19:42:27 +0900

    design : 토요일, 일요일 색상 변경, date-fns 라이브러리 설치

--- FRONTEND/src/components/CustomCalender.tsx
+++ FRONTEND/src/components/CustomCalender.tsx
@@ -3,6 +3,7 @@ import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import moment from "moment";
 import "./CustomCalender.css";
+import { isSaturday, isSunday } from "date-fns";
 
 type ValuePiece = Date | null;
 
@@ -26,6 +27,13 @@ function CustomCalender() {
     setNowDate(moment(selectedDate).format("YYYY년 MM월 DD일"));
   };
 
+  //토요일, 일요일 색상 구분을 위한 함수
+  const tileClassName = ({ date, view }) => {
+    if (view === "month") {
+      if (isSaturday(date)) return "saturday";
+      if (isSunday(date)) return "sunday";
+    }
+  };
   return (
     <div>
       <header>
@@ -39,6 +47,7 @@ function CustomCalender() {
             value={value}
             formatDay={(locale, date) => moment(date).format("DD")}
             formatMonthYear={formatMonthYear}
+            tileClassName={tileClassName}
           ></Calendar>
         </main>
       </div>
