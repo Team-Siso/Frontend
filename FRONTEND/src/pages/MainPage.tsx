1ce7710fb3488cd7651e0b627ce064cd8065740e
Shin-Sujin <tnwls5621@tukorea.ac.kr>
2024-04-27 17:08:31 +0900

    design:사이드바, 캘린더 구역 나눔

--- FRONTEND/src/pages/MainPage.tsx
+++ FRONTEND/src/pages/MainPage.tsx
@@ -2,19 +2,21 @@ import React from "react";
 import Custom from "../components/Calendar/CustomCalendar";
 import FriendsListSidebar from "../components/FriendsListSidebar/FriendsListSidebar";
 import ListSidebar from "../components/ListSidebar/ListSidebar";
-import CalendarEx from "../components/Calendar/CalendarEx";
+import CalendarPage from "../components/Calendar/CalendarPage";
+
+// 각 컴포넌트마다 차지하는 공간을 알아보려구 우선 핑크색, 노란색, 회색으로 배경을 설정했다..
 
 const MainPage = () => {
   return (
-    <div style={{ display: "flex", flexDirection: "row", height: "100vh", width: "100vw" }}>
-      <div style={{ flex: "1", maxWidth: "5%" }}>
+    <div className="flex w-full h-screen overflow-hidden">
+      <div className="bg-pink-500 flex-none" style={{ width: "5%" }}>
         <FriendsListSidebar />
       </div>
-      <div style={{ flex: "5", maxWidth: "25%" }}>
+      <div className="bg-yellow-400 flex-none" style={{ width: "25%" }}>
         <ListSidebar />
       </div>
-      <div style={{ flex: "14", maxWidth: "70%" }}>
-        <CalendarEx />
+      <div className="bg-gray-300 flex-auto">
+        <CalendarPage />
       </div>
     </div>
   );
