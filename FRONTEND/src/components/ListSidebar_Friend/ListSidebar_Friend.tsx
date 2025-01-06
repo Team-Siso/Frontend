17c40f5472ec192d7a2441f72d4dbaef03649fe0
Shin-Sujin <tnwls5621@tukorea.ac.kr>
2025-01-06 16:55:11 +0900

    design : 친구페이지에서 마이페이지로 이동 버튼

--- FRONTEND/src/components/ListSidebar_Friend/ListSidebar_Friend.tsx
+++ FRONTEND/src/components/ListSidebar_Friend/ListSidebar_Friend.tsx
@@ -1,22 +1,39 @@
+import { useNavigate } from "react-router-dom"; // useNavigate 훅을 import
 import TodayComponent from "./TodayComponent";
 import TodoListComponent_Friend from "./TodoListComponent_Friend";
 import MyGoalComponent_Friend from "./MyGoalComponent_Friend";
 import MyProfileComponent_Friend from "./MyProfileComponent_Friend";
 
-const ListSidebar_Friend = () => (
-  <div className="flex flex-col h-screen">
-    <TodayComponent className="flex-none h-60" />{" "}
-    <div className="flex-grow flex flex-col">
-      {/* TodoListComponent와 MyGoalComponent의 높이를 계산하여 설정 */}
-      <div className="flex-none" style={{ height: "calc((100vh - 60px - 120px) / 2)" }}>
-        <TodoListComponent_Friend className="flex-none" />
+const ListSidebar_Friend = () => {
+  const navigate = useNavigate(); // navigate 훅 사용
+
+  const goToMyPage = () => {
+    navigate("/main"); // /main 경로로 이동
+  };
+
+  return (
+    <div className="flex flex-col h-screen">
+      <div className="flex items-center justify-between">
+        <TodayComponent className="flex-none h-60" />
+        <button
+          onClick={goToMyPage} // /main 경로로 이동하는 이벤트 핸들러
+          className="bg-purple-300 text-white text-sm py-2 px-4 rounded-full mr-4"
+        >
+          My page
+        </button>
+      </div>{" "}
+      <div className="flex-grow flex flex-col">
+        {/* TodoListComponent와 MyGoalComponent의 높이를 계산하여 설정 */}
+        <div className="flex-none" style={{ height: "calc((100vh - 60px - 120px) / 2)" }}>
+          <TodoListComponent_Friend className="flex-none" />
+        </div>
+        <div className="flex-none" style={{ height: "calc((100vh - 60px - 120px) / 2)" }}>
+          <MyGoalComponent_Friend className="flex-none" />
+        </div>
       </div>
-      <div className="flex-none" style={{ height: "calc((100vh - 60px - 120px) / 2)" }}>
-        <MyGoalComponent_Friend className="flex-none" />
-      </div>
+      <MyProfileComponent_Friend className="flex-none h-120" />
     </div>
-    <MyProfileComponent_Friend className="flex-none h-120" />
-  </div>
-);
+  );
+};
 
 export default ListSidebar_Friend;
